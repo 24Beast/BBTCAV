fcaa622f635a32e5436a4247afaa8a84d4149111
Bhanu Tokas <bhanu.dharam.tokas@gmail.com>
2025-08-26 16:41:40 -0700

    Added CBM training code.

--- utils/CBM_trainer.py
+++ utils/CBM_trainer.py
@@ -9,12 +9,12 @@ from torch.utils.data import DataLoader
 from utils.CBM import ConceptBottleneckModel
 
 # Constants
-B_SIZE = 2048
+B_SIZE = 4096
 NUM_EPOCHS = 5
 LR = 0.001
 DATA_DIR = "../Datasets/CelebA/"
 SAVE_DIR = "./models/"
-MODEL_NAME = "celebA_CNN.pth"
+MODEL_NAME = "celebA_CBM.pth"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 ALPHA = 0.05
 
@@ -35,18 +35,16 @@ train_data = CelebAJointConcept(
     split="train",
     target=target_attr,
     concept=concepts,
-    mode="concept",
     transform=transform,
-    concept_num=20000,
+    concept_num=5000,
 )
 val_data = CelebAJointConcept(
     data_dir=DATA_DIR,
     split="valid",
     target=target_attr,
     concept=concepts,
-    mode="concept",
     transform=transform,
-    concept_num=4000,
+    concept_num=1000,
 )
 
 train_loader = DataLoader(train_data, batch_size=B_SIZE)
@@ -56,8 +54,7 @@ val_loader = DataLoader(val_data, batch_size=B_SIZE)
 # Model Definition
 print("Preparing for training...")
 
-model = ConceptBottleneckModel(len(concepts), 1, pretrained=True)
-
+model = ConceptBottleneckModel(len(concepts), 1, pretrained=True).to(DEVICE)
 criterion = nn.BCEWithLogitsLoss()
 optimizer = optim.Adam(model.parameters(), lr=LR)
 
@@ -65,13 +62,13 @@ for epoch in range(NUM_EPOCHS):
     model.train()
     total_loss = 0
     n = len(train_loader)
-    for i, (imgs, labels, concepts) in enumerate(train_loader):
+    for i, (imgs, concepts, labels) in enumerate(train_loader):
         print(f"\rWorking on batch {i}/{n}", end="")
         imgs, labels = imgs.to(DEVICE), labels.to(DEVICE).unsqueeze(1).float()
         concepts = concepts.to(DEVICE)
-        pred_labels, pred_concepts = model(imgs, return_intermediate=True)
+        _, pred_concepts, pred_labels = model(imgs, return_intermediate=True)
         loss_pred = criterion(pred_labels, labels)
-        loss_concepts = criterion(pred_concepts, concepts)
+        loss_concepts = criterion(pred_concepts, concepts.float())
         loss = loss_pred + ALPHA * loss_concepts
 
         optimizer.zero_grad()
@@ -82,17 +79,30 @@ for epoch in range(NUM_EPOCHS):
     avg_loss = total_loss / len(train_loader.dataset)
     print(f"\nEpoch {epoch+1}/{NUM_EPOCHS}, Loss: {avg_loss:.4f}")
     model.eval()
-    correct = 0
+    correct_labels = 0
+    correct_concepts = 0
     total = 0
     with torch.no_grad():
-        for imgs, labels, concepts in val_loader:
+        for imgs, concepts, labels in val_loader:
             imgs, labels = imgs.to(DEVICE), labels.to(DEVICE).unsqueeze(1).float()
-            outputs = model(imgs)
-            preds = torch.sigmoid(outputs) > 0.5  # Convert logits to binary predictions
-            correct += (preds.float() == labels).sum().item()
+            _, pred_concepts, pred_labels = model(imgs, return_intermediate=True)
+            concepts = concepts.to(DEVICE)
+            pred_labels = (
+                torch.sigmoid(pred_labels) > 0.5
+            )  # Convert logits to binary predictions
+            correct_labels = (pred_labels.float() == labels).sum().item()
+            pred_concepts = (
+                torch.sigmoid(pred_concepts) > 0.5
+            )  # Convert logits to binary predictions
+            correct_concepts += (
+                (pred_concepts.float() == concepts).float().mean(axis=1).item()
+            )
             total += labels.size(0)
-    val_acc = correct / total
-    print(f"Validation Accuracy: {val_acc:.4f}")
+    val_l_acc = correct_labels / total
+    val_c_acc = correct_concepts / total
+    print(
+        f"Validation Label Accuracy: {val_l_acc:.4f}| Validation Concept Accuracy: {val_c_acc:.4f}"
+    )
 
 # === Save ===
 if not (os.path.isdir(SAVE_DIR)):
